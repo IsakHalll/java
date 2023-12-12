db7802cb784502e27246cb6e55be721390fe2533
IsakHalll <isak.akerberg@elev.ga.ntig.se>
2023-12-12 10:17:00 +0100

    java script

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hej")
+const btnHello = document.getElementById("demmo")
+const btn = document.getElementById("button")
+function myFunctionn() {
+    btnHello.innerHTML = "Hej Nicke";
+    btn.classList.toggle("newbackground")
+  }
+function myFunction() {
+alert("hej hej");
+}
+
